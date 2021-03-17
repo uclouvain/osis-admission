8a2e0b44ef473930e7f7f19ff2bf0710707ab1a2
jmr <jmr@makina-corpus.com>
2021-03-17 12:16:32 +0100

    Base admission model

---
+++ models/enums/__init__.py
@@ -0,0 +1,1 @@
+from .admission import *
---
+++ models/enums/admission.py
@@ -0,0 +1,8 @@
+from base.models.utils.utils import ChoiceEnum
+
+from django.utils.translation import gettext_lazy as _
+
+
+class AdmissionType(ChoiceEnum):
+    ADMISSION = _("Admission")
+    PRE_ADMISSION = _("Pré-admission")
