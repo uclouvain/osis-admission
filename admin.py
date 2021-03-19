32a4a7fd22e346f6d947f82390e1d786fec91438
jmr <jmr@makina-corpus.com>
2021-03-19 15:34:41 +0100

    Save author on admin save form

--- admin.py
+++ admin.py
@@ -2,4 +2,16 @@ from django.contrib import admin
 
 from admission.contrib.models import AdmissionDoctorate
 
-admin.site.register(AdmissionDoctorate)
+
+class AdmissionDoctorateAdmin(admin.ModelAdmin):
+    def save_form(self, request, form, change):
+        """
+        Set the author if the admission doctorate is being created
+        """
+        admission_doctorate = form.save(commit=False)
+        if not change:
+            admission_doctorate.author = request.user.person
+        return admission_doctorate
+
+
+admin.site.register(AdmissionDoctorate, AdmissionDoctorateAdmin)
