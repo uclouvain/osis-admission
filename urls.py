de58f0d3327015f39b0a1b21e4f1b47439acf38a
jmr <jmr@makina-corpus.com>
2021-03-19 15:36:31 +0100

    Renaming "doctorats" into "doctorates"

--- urls.py
+++ urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from admission.contrib.views import AdmissionDoctorateListView
+from .contrib.views import AdmissionDoctorateListView
 
 
 app_name = "admissions"
 urlpatterns = [
-    path('doctorats', AdmissionDoctorateListView.as_view(), name="doctorate-list"),
+    path('doctorates/', AdmissionDoctorateListView.as_view(), name="doctorate-list"),
 ]
