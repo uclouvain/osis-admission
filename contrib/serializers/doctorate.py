173ad13cd34e81768f7a2a9bb439efa6ecc6e067
jmr <jmr@makina-corpus.com>
2021-03-22 10:49:46 +0100

    Person serializer and type admission display

--- contrib/serializers/doctorate.py
+++ contrib/serializers/doctorate.py
@@ -1,9 +1,22 @@
 from rest_framework import serializers
 
 from admission.contrib.models import AdmissionDoctorate
+from base.models.person import Person
+
+
+class PersonSerializer(serializers.ModelSerializer):
+    str = serializers.CharField(source="__str__")
+
+    class Meta:
+        model = Person
+        fields = ['str', ]
 
 
 class AdmissionDoctorateSerializer(serializers.ModelSerializer):
+    type = serializers.CharField(source="get_type_display")
+    candidate = PersonSerializer()
+    author = PersonSerializer()
+
     class Meta:
         model = AdmissionDoctorate
         fields = [
