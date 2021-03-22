8b8c9e6d63d6ae583fc71e63e0ebbf6e75156ae8
jmr <jmr@makina-corpus.com>
2021-03-22 11:04:40 +0100

    person autocomplete

--- urls.py
+++ urls.py
@@ -1,9 +1,18 @@
-from django.urls import path
+from django.urls import path, include
 
-from .contrib.views import AdmissionDoctorateListView
-
+from .contrib.views import AdmissionDoctorateListView, autocomplete
 
 app_name = "admissions"
 urlpatterns = [
     path('doctorates/', AdmissionDoctorateListView.as_view(), name="doctorate-list"),
+
+    path('autocomplete/', include(
+        [
+            path(
+                "person/",
+                autocomplete.PersonAutocomplete.as_view(),
+                name="person_autocomplete",
+            ),
+        ]
+    )),
 ]
