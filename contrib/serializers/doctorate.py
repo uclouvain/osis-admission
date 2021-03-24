bea48cf04ea3e441db0c28ad1359bfb0b5a2ce83
jmr <jmr@makina-corpus.com>
2021-03-24 14:14:59 +0100

    Replacing PersonSerializer by a StringRelatedField

--- contrib/serializers/doctorate.py
+++ contrib/serializers/doctorate.py
@@ -1,22 +1,13 @@
 from rest_framework import serializers
 
 from admission.contrib.models import AdmissionDoctorate
-from base.models.person import Person
-
-
-class PersonSerializer(serializers.ModelSerializer):
-    str = serializers.CharField(source="__str__")
-
-    class Meta:
-        model = Person
-        fields = ['str', ]
 
 
 class AdmissionDoctorateSerializer(serializers.ModelSerializer):
     url = serializers.URLField(source="get_absolute_url")
     type = serializers.CharField(source="get_type_display")
-    candidate = PersonSerializer()
-    author = PersonSerializer()
+    candidate = serializers.StringRelatedField()
+    author = serializers.StringRelatedField()
 
     class Meta:
         model = AdmissionDoctorate
