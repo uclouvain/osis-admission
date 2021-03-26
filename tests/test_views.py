002aefc90cdeb677b48f465dc5a523d534f61b85
jmr <jmr@makina-corpus.com>
2021-03-26 15:07:12 +0100

    Limit the queryset for admission doctorates candidates to person that have admissions

--- tests/test_views.py
+++ tests/test_views.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 
 from admission.contrib.models import AdmissionDoctorate, AdmissionType
 from admission.contrib.views.autocomplete import PersonAutocomplete
+from admission.tests.factories import AdmissionDoctorateFactory
 from base.tests.factories.person import PersonFactory
 from base.tests.factories.user import UserFactory
 
@@ -11,11 +12,20 @@ class PersonAutocompleteViewTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.user = UserFactory()
+        cls.author = PersonFactory(user=cls.user)
         cls.candidate = PersonFactory()
         cls.url = reverse("admissions:person_autocomplete")
 
-    def test_filter_autocomplete_returns_existing_user(self):
+    def test_filter_autocomplete_doesnt_return_candidates_with_no_admissions(self):
         self.client.force_login(self.user)
+        response = self.client.get(self.url, data={"q": self.candidate.email})
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertEqual(len(data["results"]), 0)
+
+    def test_filter_autocomplete_returns_candidates_with_admissions(self):
+        self.client.force_login(self.user)
+        AdmissionDoctorateFactory(candidate=self.candidate, author=self.author)
         response = self.client.get(self.url, data={"q": self.candidate.email})
         self.assertEqual(response.status_code, 200)
         data = response.json()
