73c74e1df190b0b32d601458eccda346640bdb02
jmr <jmr@makina-corpus.com>
2021-04-06 14:11:51 +0200

    Trying to fix the travis build bug returning a RuntimeError
        See https://travis-ci.com/github/uclouvain/osis-admission/builds/222238595

--- contrib/models/__init__.py
+++ contrib/models/__init__.py
@@ -1,6 +1,15 @@
-from .doctorate import AdmissionDoctorate
-from .enums.admission_type import AdmissionType
+try:
+    from .doctorate import AdmissionDoctorate
+    from .enums.admission_type import AdmissionType
 
-__all__ = [
-    "AdmissionDoctorate", "AdmissionType"
-]
+    __all__ = [
+        "AdmissionDoctorate", "AdmissionType"
+    ]
+
+except RuntimeError as e:  # pragma: no cover
+    # There's a weird bug when running tests, the test runner seeing a models
+    # package tries to import it directly, failing to do so
+    import sys
+
+    if 'test' not in sys.argv:
+        raise e
