2ac95ee97831375ccc42538c845189cfa051b294
jmr <jmr@makina-corpus.com>
2021-11-19 17:26:36 +0100

    demander signature API view

--- api/url_v1.py
+++ api/url_v1.py
@@ -41,6 +41,7 @@ urlpatterns = [
     path('propositions/<uuid:uuid>', views.PropositionViewSet),
     path('propositions/<uuid:uuid>/cotutelle', views.CotutelleAPIView),
     path('propositions/<uuid:uuid>/supervision', views.SupervisionAPIView),
+    path('propositions/<uuid:uuid>/request_signatures', views.RequestSignaturesAPIView),
     path('autocomplete/sector', views.AutocompleteSectorView),
     path('autocomplete/sector/<str:sigle>/doctorates', views.AutocompleteDoctoratView),
     path('autocomplete/tutor', views.AutocompleteTutorView),
