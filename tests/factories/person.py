93e8499236bc3a9a0d2f0b0b0a235f6a7c95995f
Julien Cougnaud <jco@makina-corpus.net>
2022-02-24 15:12:31 +0100

    Add license block

--- tests/factories/person.py
+++ tests/factories/person.py
@@ -1,3 +1,28 @@
+# ##############################################################################
+#
+#    OSIS stands for Open Student Information System. It's an application
+#    designed to manage the core business of higher education institutions,
+#    such as universities, faculties, institutes and professional schools.
+#    The core business involves the administration of students, teachers,
+#    courses, programs and so on.
+#
+#    Copyright (C) 2015-2022 Université catholique de Louvain (http://www.uclouvain.be)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    A copy of this license - GNU General Public License - is available
+#    at the root of the source code of this program.  If not,
+#    see http://www.gnu.org/licenses/.
+#
+# ##############################################################################
 import operator
 
 import factory
@@ -12,17 +37,6 @@ from base.tests.factories.person import PersonFactory
 from osis_profile.tests.factories.curriculum import CurriculumYearFactory
 from reference.tests.factories.country import CountryFactory
 from reference.tests.factories.language import FrenchLanguageFactory, EnglishLanguageFactory
-
-
-class PersonAddressFactory(factory.DjangoModelFactory):
-
-    class Meta:
-        model = 'base.PersonAddress'
-
-    street = factory.Faker('street_name')
-    postal_code = factory.Faker('zipcode')
-    city = factory.Faker('city')
-    country = factory.SubFactory(CountryFactory)
 
 
 class PersonAddressFactory(factory.DjangoModelFactory):
