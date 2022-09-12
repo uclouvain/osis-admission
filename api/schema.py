d8af9d466f60f32465c342bdb9f3488a1c2a6eb1
Julien Cougnaud <julien.cougnaud@makina-corpus.com>
2022-09-12 17:38:55 +0200

    [OS-255] Change the equivalence files in the education tab

--- api/schema.py
+++ api/schema.py
@@ -34,7 +34,7 @@ from rest_framework.serializers import Serializer
 from admission.api.serializers.fields import ActionLinksField
 from base.models.utils.utils import ChoiceEnum
 
-ADMISSION_SDK_VERSION = "1.0.36"
+ADMISSION_SDK_VERSION = "1.0.37"
 
 
 class AdmissionSchemaGenerator(SchemaGenerator):
