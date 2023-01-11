b13927f94f7630bd70b54fb6778085c9f8a9cd78
Sebastien Corbin <seb.corbin@gmail.com>
2023-01-11 11:50:08 +0100

    Hotfix: BR not a language know from test provider

--- ddd/admission/formation_generale/test/use_case/read/test_verifier_proposition_service.py
+++ ddd/admission/formation_generale/test/use_case/read/test_verifier_proposition_service.py
@@ -1,26 +1,26 @@
 # ##############################################################################
 #
-#    OSIS stands for Open Student Information System. It's an application
-#    designed to manage the core business of higher education institutions,
-#    such as universities, faculties, institutes and professional schools.
-#    The core business involves the administration of students, teachers,
-#    courses, programs and so on.
+#  OSIS stands for Open Student Information System. It's an application
+#  designed to manage the core business of higher education institutions,
+#  such as universities, faculties, institutes and professional schools.
+#  The core business involves the administration of students, teachers,
+#  courses, programs and so on.
 #
-#    Copyright (C) 2015-2022 Université catholique de Louvain (http://www.uclouvain.be)
+#  Copyright (C) 2015-2023 Université catholique de Louvain (http://www.uclouvain.be)
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
 #
-#    A copy of this license - GNU General Public License - is available
-#    at the root of the source code of this program.  If not,
-#    see http://www.gnu.org/licenses/.
+#  A copy of this license - GNU General Public License - is available
+#  at the root of the source code of this program.  If not,
+#  see http://www.gnu.org/licenses/.
 #
 # ##############################################################################
 import datetime
@@ -1361,7 +1361,7 @@ class TestVerifierPropositionService(TestCase):
             diplome_etudes_secondaires=GotDiploma.YES.name,
             annee_diplome_etudes_secondaires=2020,
             diplome_etranger=DiplomeEtrangerEtudesSecondairesDTO(
-                regime_linguistique='BR',
+                regime_linguistique='SV',
                 type_diplome=ForeignDiplomaTypes.EUROPEAN_BACHELOR.name,
                 pays_membre_ue=True,
                 pays_iso_code=FR_ISO_CODE,
@@ -1379,7 +1379,7 @@ class TestVerifierPropositionService(TestCase):
             diplome_etudes_secondaires=GotDiploma.YES.name,
             annee_diplome_etudes_secondaires=2020,
             diplome_etranger=DiplomeEtrangerEtudesSecondairesDTO(
-                regime_linguistique='BR',
+                regime_linguistique='SV',
                 type_diplome=ForeignDiplomaTypes.EUROPEAN_BACHELOR.name,
                 pays_membre_ue=True,
                 pays_iso_code=FR_ISO_CODE,
@@ -1397,7 +1397,7 @@ class TestVerifierPropositionService(TestCase):
             diplome_etudes_secondaires=GotDiploma.THIS_YEAR.name,
             annee_diplome_etudes_secondaires=2020,
             diplome_etranger=DiplomeEtrangerEtudesSecondairesDTO(
-                regime_linguistique='BR',
+                regime_linguistique='SV',
                 type_diplome=ForeignDiplomaTypes.EUROPEAN_BACHELOR.name,
                 pays_membre_ue=True,
                 pays_iso_code=FR_ISO_CODE,
@@ -1415,7 +1415,7 @@ class TestVerifierPropositionService(TestCase):
             diplome_etudes_secondaires=GotDiploma.THIS_YEAR.name,
             annee_diplome_etudes_secondaires=2020,
             diplome_etranger=DiplomeEtrangerEtudesSecondairesDTO(
-                regime_linguistique='BR',
+                regime_linguistique='SV',
                 type_diplome=ForeignDiplomaTypes.EUROPEAN_BACHELOR.name,
                 pays_membre_ue=True,
                 pays_iso_code=FR_ISO_CODE,
@@ -1453,7 +1453,7 @@ class TestVerifierPropositionService(TestCase):
             self.experience_academiques_complete,
             releve_notes=[],
             pays=FR_ISO_CODE,
-            regime_linguistique='BR',
+            regime_linguistique='SV',
         ):
             self.experiences_academiques.append(self.experience_academiques_complete)
 
@@ -1480,7 +1480,7 @@ class TestVerifierPropositionService(TestCase):
             self.experience_academiques_complete,
             type_releve_notes=TranscriptType.ONE_A_YEAR.name,
             pays=FR_ISO_CODE,
-            regime_linguistique='BR',
+            regime_linguistique='SV',
         ):
             with mock.patch.multiple(self.experience_academiques_complete.annees[0], traduction_releve_notes=[]):
                 self.experiences_academiques.append(self.experience_academiques_complete)
--- tests/api/views/test_curriculum.py
+++ tests/api/views/test_curriculum.py
@@ -1,26 +1,26 @@
 # ##############################################################################
 #
-#    OSIS stands for Open Student Information System. It's an application
-#    designed to manage the core business of higher education institutions,
-#    such as universities, faculties, institutes and professional schools.
-#    The core business involves the administration of students, teachers,
-#    courses, programs and so on.
+#  OSIS stands for Open Student Information System. It's an application
+#  designed to manage the core business of higher education institutions,
+#  such as universities, faculties, institutes and professional schools.
+#  The core business involves the administration of students, teachers,
+#  courses, programs and so on.
 #
-#    Copyright (C) 2015-2022 Université catholique de Louvain (http://www.uclouvain.be)
+#  Copyright (C) 2015-2023 Université catholique de Louvain (http://www.uclouvain.be)
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
 #
-#    A copy of this license - GNU General Public License - is available
-#    at the root of the source code of this program.  If not,
-#    see http://www.gnu.org/licenses/.
+#  A copy of this license - GNU General Public License - is available
+#  at the root of the source code of this program.  If not,
+#  see http://www.gnu.org/licenses/.
 #
 # ##############################################################################
 import datetime
@@ -164,7 +164,7 @@ class DoctorateCurriculumTestCase(BaseCurriculumTestCase, APITestCase):
         cls.other_user = cls.other_admission.candidate.user
         cls.user_without_admission = CandidateFactory().person.user
         cls.linguistic_regime = LanguageFactory(code=FR_ISO_CODE)
-        cls.linguistic_regime_with_translation = LanguageFactory(code='BR')
+        cls.linguistic_regime_with_translation = LanguageFactory(code='SV')
 
         AdmissionFormItemInstantiationFactory(
             form_item=TextAdmissionFormItemFactory(
@@ -555,7 +555,7 @@ class GeneralEducationCurriculumTestCase(BaseCurriculumTestCase, APITestCase):
         )
 
         cls.linguistic_regime = LanguageFactory(code=FR_ISO_CODE)
-        cls.linguistic_regime_with_translation = LanguageFactory(code='BR')
+        cls.linguistic_regime_with_translation = LanguageFactory(code='SV')
 
         cls.put_data = {
             'reponses_questions_specifiques': {
