b4dea12c1db523b1441dd9752b712fcd011489e3
Sebastien Corbin <seb.corbin@gmail.com>
2023-01-24 10:24:51 +0100

    No conditions equals title access

--- ddd/admission/domain/service/i_titres_acces.py
+++ ddd/admission/domain/service/i_titres_acces.py
@@ -1,26 +1,26 @@
 # ##############################################################################
 #
-#    OSIS stands for Open Student Information System. It's an application
-#    designed to manage the core business of higher education institutions,
-#    such as universities, faculties, institutes and professional schools.
-#    The core business involves the administration of students, teachers,
-#    courses, programs and so on.
+#  OSIS stands for Open Student Information System. It's an application
+#  designed to manage the core business of higher education institutions,
+#  such as universities, faculties, institutes and professional schools.
+#  The core business involves the administration of students, teachers,
+#  courses, programs and so on.
 #
-#    Copyright (C) 2015-2022 Université catholique de Louvain (http://www.uclouvain.be)
+#  Copyright (C) 2015-2023 Université catholique de Louvain (http://www.uclouvain.be)
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
 #
-#    A copy of this license - GNU General Public License - is available
-#    at the root of the source code of this program.  If not,
-#    see http://www.gnu.org/licenses/.
+#  A copy of this license - GNU General Public License - is available
+#  at the root of the source code of this program.  If not,
+#  see http://www.gnu.org/licenses/.
 #
 # ##############################################################################
 from enum import Enum
@@ -49,10 +49,10 @@ class ConditionAccess(str, Enum):
 class Titres:
     def __init__(self, obj: AdmissionConditionsDTO, *conditions: ConditionAccess):
         self.condition_obj = obj
-        self.condition_names = conditions or tuple(e for e in ConditionAccess)
+        self.condition_names = conditions
 
     def __bool__(self):
-        return any(self.get_valid_conditions())
+        return not self.condition_names or any(self.get_valid_conditions())
 
     def __str__(self):  # pragma: no cover
         return ','.join([cond.name for cond in self.get_valid_conditions()])
