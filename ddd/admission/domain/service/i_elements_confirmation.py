2014091cf6294013e570cf668d1a83030d00d432
Sebastien Corbin <seb.corbin@gmail.com>
2023-01-24 16:25:38 +0100

    Fix year formatting when deadline reached

--- ddd/admission/domain/service/i_elements_confirmation.py
+++ ddd/admission/domain/service/i_elements_confirmation.py
@@ -6,7 +6,7 @@
 #  The core business involves the administration of students, teachers,
 #  courses, programs and so on.
 #
-#  Copyright (C) 2015-2022 Université catholique de Louvain (http://www.uclouvain.be)
+#  Copyright (C) 2015-2023 Université catholique de Louvain (http://www.uclouvain.be)
 #
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -138,7 +138,7 @@ class IElementsConfirmation(interface.DomainService):
             elements.append(
                 ElementConfirmation(
                     nom='hors_delai',
-                    texte=cls.HORS_DELAI % {'year': f"{proposition.annee_calculee}-{proposition.annee_calculee - 1}"},
+                    texte=cls.HORS_DELAI % {'year': f"{proposition.annee_calculee}-{proposition.annee_calculee + 1}"},
                 )
             )
         elements += [
