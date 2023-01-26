6c7333b729eda70b7e750120669aef49597b9da3
Sebastien Corbin <seb.corbin@gmail.com>
2023-01-26 10:38:29 +0100

    Update SDK version

--- api/schema.py
+++ api/schema.py
@@ -34,7 +34,7 @@ from rest_framework.serializers import Serializer
 from admission.api.serializers.fields import ActionLinksField
 from base.models.utils.utils import ChoiceEnum
 
-ADMISSION_SDK_VERSION = "1.0.61"
+ADMISSION_SDK_VERSION = "1.0.62"
 
 
 class AdmissionSchemaGenerator(SchemaGenerator):
