ef8281983f38dfdc429544fef4474b09d3a5cd36
Julien Cougnaud <julien.cougnaud@makina-corpus.com>
2023-04-28 11:15:18 +0200

    [OS-442] Improve & fix the tab header and add historic for the general education admissions

--- ddd/admission/formation_generale/use_case/write/soumettre_proposition_service.py
+++ ddd/admission/formation_generale/use_case/write/soumettre_proposition_service.py
@@ -28,6 +28,7 @@ import datetime
 from admission.ddd.admission.domain.builder.formation_identity import FormationIdentityBuilder
 from admission.ddd.admission.domain.service.i_calendrier_inscription import ICalendrierInscription
 from admission.ddd.admission.domain.service.i_elements_confirmation import IElementsConfirmation
+from admission.ddd.admission.domain.service.i_historique import IHistorique
 from admission.ddd.admission.domain.service.i_maximum_propositions import IMaximumPropositionsAutorisees
 from admission.ddd.admission.domain.service.i_profil_candidat import IProfilCandidatTranslator
 from admission.ddd.admission.domain.service.i_titres_acces import ITitresAcces
@@ -63,6 +64,7 @@ def soumettre_proposition(
     notification: 'INotification',
     maximum_propositions_service: 'IMaximumPropositionsAutorisees',
     inscription_tardive_service: 'IInscriptionTardive',
+    historique: 'IHistorique',
 ) -> 'PropositionIdentity':
     # GIVEN
     proposition_id = PropositionIdentityBuilder.build_from_uuid(cmd.uuid_proposition)
@@ -123,5 +125,6 @@ def soumettre_proposition(
     proposition.soumettre(formation_id, pool, cmd.elements_confirmation, type_demande, est_inscription_tardive)
     proposition_repository.save(proposition)
     notification.confirmer_soumission(proposition)
+    historique.historiser_soumission(proposition)
 
     return proposition_id
