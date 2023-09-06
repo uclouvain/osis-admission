36a3589f5ddfca2a86d1718e055790112ce1ad2f
Gagaro <gagaro42@gmail.com>
2023-09-06 15:49:03 +0200

    Fix OS-586: Front-office : Élargir la recherche de l'année effective au calendrier N+2

--- infrastructure/admission/domain/service/in_memory/calendrier_inscription.py
+++ infrastructure/admission/domain/service/in_memory/calendrier_inscription.py
@@ -6,7 +6,7 @@
 #    The core business involves the administration of students, teachers,
 #    courses, programs and so on.
 #
-#    Copyright (C) 2015-2022 Université catholique de Louvain (http://www.uclouvain.be)
+#    Copyright (C) 2015-2023 Université catholique de Louvain (http://www.uclouvain.be)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -45,7 +45,7 @@ class CalendrierInscriptionInMemory(ICalendrierInscription):
     @classmethod
     def get_annees_academiques_pour_calcul(cls) -> List[int]:
         current_year = get_current_year()
-        return [current_year, current_year - 1, current_year + 1]
+        return [current_year, current_year - 1, current_year + 1, current_year + 2]
 
     @classmethod
     def get_pool_ouverts(cls) -> List[Tuple[str, int]]:
