2d26a1559b3f440263d41b4e5dbbb0cc76cce689
Julien Cougnaud <julien.cougnaud@makina-corpus.com>
2023-10-06 13:09:22 +0200

    Use the ActionLinksFields class from base

--- api/serializers/dashboard.py
+++ api/serializers/dashboard.py
@@ -6,7 +6,7 @@
 #    The core business involves the administration of students, teachers,
 #    courses, programs and so on.
 #
-#    Copyright (C) 2015-2022 Université catholique de Louvain (http://www.uclouvain.be)
+#    Copyright (C) 2015-2023 Université catholique de Louvain (http://www.uclouvain.be)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -26,7 +26,8 @@
 
 from rest_framework import serializers
 
-from admission.api.serializers.fields import ACTION_LINKS, ActionLinksField
+from admission.api.serializers.fields import ACTION_LINKS
+from backoffice.settings.rest_framework.fields import ActionLinksField
 
 
 class DashboardSerializer(serializers.Serializer):
