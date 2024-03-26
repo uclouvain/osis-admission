fe146b3f585b5cdf31b33ff9222e7d2b5b9a9b4f
Julien Cougnaud <julien.cougnaud@makina-corpus.com>
2024-03-26 15:58:27 +0100

    [OS-926] Admission header > retrieve an html link instead of a simple link

--- static/admission/clipboard.js
+++ static/admission/clipboard.js
@@ -25,40 +25,91 @@
  */
 
 /**
- * Write text to clipboard when the user clicks on a button (requires jquery + tooltip.js from bootstrap)
- * @param clipboardSelector
- * @param resetTitleAfter Reset the button title after a specific duration (in s)
+ * Write content to clipboard when the user clicks on a button (requires jquery + tooltip.js from bootstrap).
+ * Each clipboard element must have a button and something having a 'copy-to-clipboard-element' class from which
+ * the content is copied.
+ * If a 'data-html' attribute is set to 'true' on the button, the content itself is copied as html, otherwise only the
+ * inner text content is copied (by default).
+ * If the browser doesn't support the clipboard API, the button selects the related content.
+ * @param clipboardSelector the selector of the clipboard elements.
  */
-function writeTextToClipboard(clipboardSelector, resetTitleAfter=3) {
-    // Each clipboard element must have a button and something having a 'copy-text' class from which the text is copied
-    const initialTooltipText = gettext('Copy to clipboard');
-    const tooltipTextOnSuccess = gettext('Copied!');
-    const tooltipTextOnFailure = gettext('Impossible to copy');
+function writeTextToClipboard(clipboardSelector) {
+    const tooltipTextsForCopy = {
+        initial: gettext('Copy to clipboard'),
+        success: gettext('Copied!'),
+        failure: gettext('Impossible to copy'),
+    }
+    const tooltipTextsForSelection = {
+        initial: gettext('Select the element'),
+        success: gettext('Selected!'),
+        failure: gettext('Impossible to select'),
+    }
+
+    const canUseClipboardWriteTextAPI = Boolean(navigator.clipboard && navigator.clipboard.writeText);
+    const canUseClipboardWriteApi = Boolean(navigator.clipboard && navigator.clipboard.write);
 
     $(clipboardSelector).each(function() {
         const buttonComponent = $(this).find('button');
+        const isHtmlContent = buttonComponent[0].dataset.html === 'true';
+        const canCopy = isHtmlContent && canUseClipboardWriteApi || !isHtmlContent && canUseClipboardWriteTextAPI;
 
-        buttonComponent.attr({
-            'title': initialTooltipText,
-            'data-toggle': 'tooltip',
-            'data-trigger': 'hover',
-        })
+        const tooltipTexts = canCopy ? tooltipTextsForCopy : tooltipTextsForSelection;
 
-        buttonComponent.on('click', () => {
-            // Copy text content to clipboard
-            const $textComponent = $(this).find('.copy-to-clipboard-text');
-            let text = $textComponent.attr('data-text');
-            if (!text) {
-                text = $textComponent.text();
-            }
-            navigator.clipboard.writeText(text).then(() => {
-                buttonComponent.attr('data-original-title', tooltipTextOnSuccess).tooltip('show');
-                buttonComponent.attr('data-original-title', initialTooltipText);
-            }, () => {
-                buttonComponent.attr('data-original-title', tooltipTextOnFailure).tooltip('show');
-                buttonComponent.attr('data-original-title', initialTooltipText);
+        buttonComponent.attr('data-original-title', tooltipTexts.initial).tooltip();
+
+        const onSuccess = () => {
+            buttonComponent.attr('data-original-title', tooltipTexts.success).tooltip('show');
+            buttonComponent.attr('data-original-title', tooltipTexts.initial);
+        }
+        const onFailure = () => {
+            buttonComponent.attr('data-original-title', tooltipTexts.failure).tooltip('show');
+            buttonComponent.attr('data-original-title', tooltipTexts.initial);
+        }
+
+        const elementToCopy = $(this).find('.copy-to-clipboard-element');
+
+        if (!elementToCopy.length) return;
+
+        if (isHtmlContent && canUseClipboardWriteApi) {
+            // Use the clipboard API to directly copy the desired html content
+            buttonComponent.on('click', () => {
+                const clipboardItem = new ClipboardItem({
+                    "text/plain": new Blob(
+                        [elementToCopy[0].innerText],
+                        {type: "text/plain"}
+                    ),
+                    "text/html": new Blob(
+                        [elementToCopy[0].outerHTML],
+                        {type: "text/html"}
+                    ),
+                });
+                navigator.clipboard.write([clipboardItem]).then(onSuccess, onFailure);
+
             });
-        });
+
+        } else if (!isHtmlContent && canUseClipboardWriteTextAPI) {
+            // Use the clipboard API to directly copy the desired text content
+            buttonComponent.on('click', () => {
+                navigator.clipboard.writeText(elementToCopy[0].innerText).then(onSuccess, onFailure);
+            });
+        }
+        else {
+            // Use the selection API to select the desired content
+            buttonComponent.on('click', () => {
+                const selection = window.getSelection();
+
+                // Clear selection
+                selection.removeAllRanges();
+
+                // Select the content
+                const range = document.createRange();
+                range.selectNodeContents(elementToCopy[0]);
+                selection.addRange(range);
+
+                // Update the tooltip
+                onSuccess();
+            });
+        }
     })
     $(clipboardSelector + ' button[data-toggle="tooltip"]').tooltip();
 }
