e3dfc20cf0ae9dd94832941c67482122c5c280d2
Julien Cougnaud <julien.cougnaud@makina-corpus.com>
2024-03-27 14:53:36 +0100

    [OS-928] Consider valid the epc cv experiences and prevent to update them through the API

--- infrastructure/admission/domain/service/in_memory/profil_candidat.py
+++ infrastructure/admission/domain/service/in_memory/profil_candidat.py
@@ -178,6 +178,7 @@ class ExperienceAcademique:
     type_institut: str
     cycle_formation: str
     nom_formation_equivalente_communaute_fr: str
+    identifiant_externe: Optional[str] = None
 
 
 @dataclass
@@ -192,6 +193,7 @@ class ExperienceNonAcademique:
     fonction: str
     secteur: str
     autre_activite: str
+    identifiant_externe: Optional[str] = None
 
 
 class ProfilCandidatInMemoryTranslator(IProfilCandidatTranslator):
