6fbb3548d41a0b9194fb8ef29346634d7ea2c6bd
Benjamin Daubry <benjamin.daubry@uclouvain.be>
2024-05-03 16:41:34 +0200

    Curex: delete url

--- tests/views/common/form_tabs/curriculum/test_educational_experience.py
+++ tests/views/common/form_tabs/curriculum/test_educational_experience.py
@@ -1,26 +1,26 @@
 # ##############################################################################
 #
-#  OSIS stands for Open Student Information System. It's an application
-#  designed to manage the core business of higher education institutions,
-#  such as universities, faculties, institutes and professional schools.
-#  The core business involves the administration of students, teachers,
-#  courses, programs and so on.
+#    OSIS stands for Open Student Information System. It's an application
+#    designed to manage the core business of higher education institutions,
+#    such as universities, faculties, institutes and professional schools.
+#    The core business involves the administration of students, teachers,
+#    courses, programs and so on.
 #
-#  Copyright (C) 2015-2024 Université catholique de Louvain (http://www.uclouvain.be)
+#    Copyright (C) 2015-2024 Université catholique de Louvain (http://www.uclouvain.be)
 #
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
 #
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
 #
-#  A copy of this license - GNU General Public License - is available
-#  at the root of the source code of this program.  If not,
-#  see http://www.gnu.org/licenses/.
+#    A copy of this license - GNU General Public License - is available
+#    at the root of the source code of this program.  If not,
+#    see http://www.gnu.org/licenses/.
 #
 # ##############################################################################
 
@@ -1580,7 +1580,8 @@ class CurriculumEducationalExperienceFormViewTestCase(TestCase):
         self.assertEqual(years[0].acquired_credit_number, 10)
         self.assertEqual(years[0].registered_credit_number, 10)
 
-    def test_post_form_with_created_experience(self):
+    @mock.patch('admission.views.common.form_tabs.curriculum.CurriculumEducationalExperienceFormView.delete_url')
+    def test_post_form_with_created_experience(self, mock_delete_url):
         self.client.force_login(self.sic_manager_user)
 
         self.assertFalse(
@@ -1668,7 +1669,8 @@ class CurriculumEducationalExperienceFormViewTestCase(TestCase):
             },
         )
 
-    def test_post_form_with_created_experience_with_blank_years(self):
+    @mock.patch('admission.views.common.form_tabs.curriculum.CurriculumEducationalExperienceFormView.delete_url')
+    def test_post_form_with_created_experience_with_blank_years(self, mock_delete_url):
         self.client.force_login(self.sic_manager_user)
 
         self.assertFalse(
