3c47e50c67b517ff30c6296f01072b2a08b8f90c
Benjamin Daubry <benjamin.daubry@uclouvain.be>
2024-07-10 16:17:43 +0200

    EPC Injection: signaletique

--- services/injection_epc/injection_dossier.py
+++ services/injection_epc/injection_dossier.py
@@ -6,7 +6,7 @@
 #    The core business involves the administration of students, teachers,
 #    courses, programs and so on.
 #
-#    Copyright (C) 2015-2023 Université catholique de Louvain (http://www.uclouvain.be)
+#    Copyright (C) 2015-2024 Université catholique de Louvain (http://www.uclouvain.be)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -23,6 +23,7 @@
 #    see http://www.gnu.org/licenses/.
 #
 # ##############################################################################
+
 import json
 import uuid
 from typing import Dict, List
@@ -38,10 +39,10 @@ from admission.contrib.models.base import (
     AdmissionProfessionalValuatedExperiences,
 )
 from admission.contrib.models.enums.actor_type import ActorType
-from admission.contrib.models.epc_injection import EPCInjectionStatus
+from admission.contrib.models.epc_injection import EPCInjectionStatus, EPCInjectionType
 from admission.contrib.models.general_education import AdmissionPrerequisiteCourses
-from admission.ddd.admission.enums import ChoixAffiliationSport, TypeSituationAssimilation
 from admission.ddd.admission.formation_generale.domain.model.enums import DROITS_INSCRIPTION_MONTANT_VALEURS
+from admission.services.injection_epc.injection_signaletique import InjectionEPCSignaletique
 from base.models.enums.person_address_type import PersonAddressType
 from base.models.enums.sap_client_creation_source import SAPClientCreationSource
 from base.models.person import Person
@@ -58,12 +59,6 @@ from osis_profile.models import (
 )
 from osis_profile.services.injection_epc import InjectionEPCCurriculum, TYPE_DIPLOME_MAP
 
-SPORT_TOUT_CAMPUS = [
-    ChoixAffiliationSport.MONS_UCL.name,
-    ChoixAffiliationSport.TOURNAI_UCL.name,
-    ChoixAffiliationSport.SAINT_GILLES_UCL.name,
-    ChoixAffiliationSport.SAINT_LOUIS_UCL.name
-]
 DOCUMENT_MAPPING = {
     'ATTESTATION_ABSENCE_DETTE_ETABLISSEMENT': 'INSTITUTE_ABSENCE_DEBTS_CERTIFICATE',
     'ATTESTATION_ENFANT_PERSONNEL': 'STAFF_CHILD_CERTIFICATE',
@@ -149,6 +144,7 @@ class InjectionEPCAdmission:
         donnees = self.recuperer_donnees(admission=admission)
         EPCInjection.objects.get_or_create(
             admission=admission,
+            type=EPCInjectionType.DEMANDE.name,
             defaults={'payload': donnees, 'status': EPCInjectionStatus.PENDING.name}
         )
         logger.info(f"[INJECTION EPC] Donnees recuperees : {json.dumps(donnees, indent=4)} - Envoi dans la queue")
@@ -163,46 +159,28 @@ class InjectionEPCAdmission:
     @classmethod
     def recuperer_donnees(cls, admission: BaseAdmission):
         candidat = admission.candidate  # Person
-        comptabilite = getattr(admission, 'accounting', None)
+        comptabilite = getattr(admission, 'accounting', None) # type: Accounting
         adresses = candidat.personaddress_set.select_related('country')
         adresse_domicile = adresses.filter(label=PersonAddressType.RESIDENTIAL.name).first()  # type: PersonAddress
         return {
             'dossier_uuid': str(admission.uuid),
-            'signaletique': cls._get_signaletique(candidat=candidat, adresse_domicile=adresse_domicile),
+            'signaletique': InjectionEPCSignaletique._get_signaletique(
+                candidat=candidat,
+                adresse_domicile=adresse_domicile,
+            ),
             'comptabilite': cls._get_comptabilite(candidat=candidat, comptabilite=comptabilite),
             'etudes_secondaires': cls._get_etudes_secondaires(candidat=candidat, admission=admission),
             'curriculum_academique': cls._get_curriculum_academique(candidat=candidat, admission=admission),
             'curriculum_autres': cls._get_curriculum_autres_activites(candidat=candidat, admission=admission),
-            'inscription_annee_academique': cls._get_inscription_annee_academique(admission=admission),
+            'inscription_annee_academique': InjectionEPCSignaletique._get_inscription_annee_academique(
+                admission=admission,
+                comptabilite=comptabilite
+            ),
             'inscription_offre': cls._get_inscription_offre(admission=admission),
             'donnees_comptables': cls._get_donnees_comptables(admission=admission),
             'adresses': cls._get_adresses(adresses=adresses),
             'documents': InjectionEPCCurriculum._recuperer_documents(admission),
             'documents_manquants': cls._recuperer_documents_manquants(admission=admission)
-        }
-
-    @classmethod
-    def _get_signaletique(cls, candidat: Person, adresse_domicile: PersonAddress) -> Dict:
-        documents = InjectionEPCCurriculum._recuperer_documents(candidat)
-        etudiant = candidat.student_set.first()
-        return {
-            'noma': etudiant.registration_id if etudiant else '',
-            'nom': candidat.last_name,
-            'prenom': candidat.first_name,
-            'prenom_suivant': candidat.middle_name,
-            'date_de_naissance': candidat.birth_date.strftime("%d/%m/%Y") if candidat.birth_date else None,
-            'lieu_de_naissance': candidat.birth_place,
-            'pays_de_naissance': candidat.birth_country.iso_code,
-            'annee_de_naissance': candidat.birth_year or None,
-            'sexe': candidat.sex,
-            'etat_civil': candidat.civil_state,
-            'numero_gsm': candidat.phone_mobile,
-            'numero_registre_national': candidat.national_number,
-            'commune_domicile': adresse_domicile.place,
-            'pays_domicile': adresse_domicile.country.iso_code,
-            'num_carte_identite': candidat.id_card_number,
-            'num_passeport': candidat.passport_number,
-            'documents': documents
         }
 
     @classmethod
@@ -364,68 +342,6 @@ class InjectionEPCAdmission:
         ]
 
     @staticmethod
-    def _get_inscription_annee_academique(admission: BaseAdmission) -> Dict:
-        candidat = admission.candidate  # type: Person
-        comptabilite = getattr(admission, 'accounting', None)  # type: Accounting
-        assimilation_checklist = admission.checklist.get('current', {}).get('assimilation', {})
-        return {
-            'annee_academique': admission.training.academic_year.year,
-            'nationalite': candidat.country_of_citizenship.iso_code,
-            'type_demande': admission.type_demande,
-            'carte_sport_lln_woluwe': (
-                comptabilite.sport_affiliation in [ChoixAffiliationSport.LOUVAIN_WOLUWE.name] + SPORT_TOUT_CAMPUS
-                if comptabilite else False
-            ),
-            'carte_sport_mons': (
-                comptabilite.sport_affiliation in [ChoixAffiliationSport.MONS.name] + SPORT_TOUT_CAMPUS
-                if comptabilite else False
-            ),
-            'carte_sport_tournai': (
-                comptabilite.sport_affiliation in [ChoixAffiliationSport.TOURNAI.name] + SPORT_TOUT_CAMPUS
-                if comptabilite else False
-            ),
-            'carte_sport_st_louis': (
-                comptabilite.sport_affiliation in [ChoixAffiliationSport.SAINT_LOUIS.name] + SPORT_TOUT_CAMPUS
-                if comptabilite else False
-            ),
-            'carte_solidaire': comptabilite.solidarity_student or False if comptabilite else False,
-            'assimilation_resident_belge': (
-                comptabilite.assimilation_situation ==
-                TypeSituationAssimilation.AUTORISATION_ETABLISSEMENT_OU_RESIDENT_LONGUE_DUREE.name
-                if comptabilite else False
-            ),
-            'assimilation_refugie': (
-                comptabilite.assimilation_situation ==
-                TypeSituationAssimilation.REFUGIE_OU_APATRIDE_OU_PROTECTION_SUBSIDIAIRE_TEMPORAIRE.name
-                if comptabilite else False
-            ),
-            'assimilation_revenus': (
-                comptabilite.assimilation_situation ==
-                TypeSituationAssimilation.AUTORISATION_SEJOUR_ET_REVENUS_PROFESSIONNELS_OU_REMPLACEMENT.name
-                if comptabilite else False
-            ),
-            'assimilation_cpas': (
-                comptabilite.assimilation_situation == TypeSituationAssimilation.PRIS_EN_CHARGE_OU_DESIGNE_CPAS.name
-                if comptabilite else False
-            ),
-            'assimilation_parents_ue': (
-                comptabilite.assimilation_situation ==
-                TypeSituationAssimilation.PROCHE_A_NATIONALITE_UE_OU_RESPECTE_ASSIMILATIONS_1_A_4.name
-                if comptabilite else False
-            ),
-            'assimilation_boursier': (
-                comptabilite.assimilation_situation == TypeSituationAssimilation.A_BOURSE_ARTICLE_105_PARAGRAPH_2.name
-                if comptabilite else False
-            ),
-            'assimilation_ue': (
-                comptabilite.assimilation_situation ==
-                TypeSituationAssimilation.RESIDENT_LONGUE_DUREE_UE_HORS_BELGIQUE.name
-                if comptabilite else False
-            ),
-            'date_assimilation': assimilation_checklist.get('extra', {}).get('date_debut', None)
-        }
-
-    @staticmethod
     def _get_inscription_offre(admission: BaseAdmission) -> Dict:
         validite, num_offre = admission.training.external_id.split('_')[4:6]
         groupe_de_supervision = getattr(admission, 'supervision_group', None)
@@ -507,10 +423,10 @@ class InjectionEPCAdmission:
                 f"[INJECTION EPC] Une erreur est survenue lors de l injection vers EPC de l admission avec uuid "
                 f"{admission_uuid} et reference {admission_reference}"
             )
-            raise InjectionVersEPCException(reference=admission_reference) from e
+            raise InjectionDemandeVersEPCException(reference=admission_reference) from e
 
 
-class InjectionVersEPCException(Exception):
+class InjectionDemandeVersEPCException(Exception):
     def __init__(self, reference: str, **kwargs):
         self.message = f"[INJECTION EPC] Impossible d injecter l admission avec reference: {reference} vers EPC"
         super().__init__(**kwargs)
@@ -524,7 +440,7 @@ def admission_response_from_epc_callback(donnees):
         f"{dossier_uuid} \nDonnees recues : {json.dumps(donnees, indent=4)}"
     )
 
-    epc_injection = EPCInjection.objects.get(admission__uuid=dossier_uuid)
+    epc_injection = EPCInjection.objects.get(admission__uuid=dossier_uuid, type=EPCInjectionType.DEMANDE.name)
     epc_injection.status = statut
     epc_injection.epc_responses.append(donnees)
     epc_injection.save()
