07de5abd05627508f0f6d9e5c0fd3b731cb884c4
Alessandro <alessandro.brugnetti@uclouvain.be>
2024-07-15 14:40:29 +0200

    Fix retrieve digit ticket status

--- tasks/retrieve_digit_tickets_status.py
+++ tasks/retrieve_digit_tickets_status.py
@@ -24,6 +24,7 @@
 #
 # ##############################################################################
 import logging
+from typing import List
 
 import waffle
 from django.conf import settings
@@ -38,6 +39,7 @@ from admission.ddd.admission.commands import (
     RecupererMatriculeDigitQuery, ModifierMatriculeCandidatCommand,
 )
 from admission.ddd.admission.domain.validator.exceptions import PasDePropositionDeFusionEligibleException
+from admission.ddd.admission.dtos.statut_ticket_personne import StatutTicketPersonneDTO
 from admission.services.injection_epc.injection_signaletique import InjectionEPCSignaletique
 from backoffice.celery import app
 from base.models.person import Person
@@ -57,7 +59,7 @@ def run(request=None):
     logger.info("Starting retrieve digit tickets status task")
 
     # Retrieve list of tickets
-    tickets_pending = message_bus_instance.invoke(command=RetrieveListeTicketsEnAttenteQuery())
+    tickets_pending = message_bus_instance.invoke(command=RetrieveListeTicketsEnAttenteQuery()) # type: List[StatutTicketPersonneDTO]
 
     logger.info("[PENDING DIGIT TICKETS] : " + str(tickets_pending))
 
@@ -67,15 +69,16 @@ def run(request=None):
         )
         logger.info(f"[DigIT Ticket] {ticket.nom}, {ticket.prenom}")
         logger.info(f"[DigIT Ticket status] {status}")
-        try:
-            person = Person.objects.get(global_id=ticket.matricule)
-        except Person.DoesNotExist:
+
+        if Person.objects.exists(global_id=ticket.matricule):
             # cas d'une creation suivie d'une fusion et d'un nouveau ticket de mise à jour
+            logger.info(f"[DigIT Ticket] Only send signaletique to EPC because already traited")
+            _injecter_signaletique_a_epc(matricule=ticket.matricule)
             continue
-        noma = person.personmergeproposal.registration_id_sent_to_digit
-        if status in ["DONE", "DONE_WITH_WARNINGS"] and noma:
+
+        if status in ["DONE", "DONE_WITH_WARNINGS"]:
             try:
-                _process_response_ticket(message_bus_instance, ticket, noma)
+                _process_response_ticket(message_bus_instance, ticket)
             except PasDePropositionDeFusionEligibleException as e:
                 logger.info(e.message)
 
@@ -85,13 +88,16 @@ def run(request=None):
 
 
 @transaction.atomic
-def _process_response_ticket(message_bus_instance, ticket, noma):
+def _process_response_ticket(message_bus_instance, ticket):
+    from admission.infrastructure.admission.repository.digit import DigitRepository
+    noma = DigitRepository.get_registration_id_sent_to_digit(global_id=ticket.matricule)
+
     digit_matricule = message_bus_instance.invoke(
         command=RecupererMatriculeDigitQuery(
             noma=noma,
         )
     )
-    logger.info(f"[DigIT Ticket noma - matricule] NOMA: {ticket.noma} - MATR: {digit_matricule}")
+    logger.info(f"[DigIT Ticket noma - matricule] NOMA: {noma} - MATR: {digit_matricule}")
 
     if Person.objects.filter(global_id=digit_matricule).exists():
         message_bus_instance.invoke(
@@ -112,13 +118,17 @@ def _process_response_ticket(message_bus_instance, ticket, noma):
         logger.info(f"[DigIT Ticket edit candidate global id]")
 
     logger.info("[DigIT Signaletique injection into EPC]")
-    demande = BaseAdmission.objects.filter(
-        submitted_at__isnull=False,
-        candidate__global_id=digit_matricule,
-    ).order_by('submitted_at').first()
-    InjectionEPCSignaletique().injecter(admission=demande)
+    _injecter_signaletique_a_epc(digit_matricule)
     send_pictures_to_card_app.run.delay(global_id=digit_matricule)
     logger.info(f"[Send picture to card]")
+
+
+def _injecter_signaletique_a_epc(matricule:str):
+    demande = BaseAdmission.objects.filter(
+        submitted_at__isnull=False,
+        candidate__global_id=matricule,
+    ).order_by('submitted_at').first()
+    InjectionEPCSignaletique().injecter(admission=demande)
 
 
 @override_switch('fusion-digit', active=True)
