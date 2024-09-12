a49f8365d9a5263a3bfba71e1ac34e9cd5ac8237
Julien Cougnaud <julien.cougnaud@makina-corpus.com>
2024-09-12 14:13:11 +0200

    [OS-600] Doctorate checklist > add the assimilation tab

--- tests/views/common/form_tabs/accounting/test_doctorate.py
+++ tests/views/common/form_tabs/accounting/test_doctorate.py
@@ -179,6 +179,8 @@ class DoctorateAccountingFormViewTestCase(TestCase):
         self.client.force_login(self.program_manager_user)
         response = self.client.get(self.form_url)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        response = self.client.post(self.form_url, data={})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_accounting_form_initialization(self):
         self.client.force_login(self.sic_manager_user)
