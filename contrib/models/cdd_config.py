f3c4e6261373c8e17927dd470f65fd1964db156c
Julien Cougnaud <julien.cougnaud@makina-corpus.com>
2024-10-10 15:53:04 +0200

    [OS-1181] Specific configuration by CDD : allow to choose a school or a faculty as entity

--- contrib/models/cdd_config.py
+++ contrib/models/cdd_config.py
@@ -6,7 +6,7 @@
 #  The core business involves the administration of students, teachers,
 #  courses, programs and so on.
 #
-#  Copyright (C) 2015-2023 Université catholique de Louvain (http://www.uclouvain.be)
+#  Copyright (C) 2015-2024 Université catholique de Louvain (http://www.uclouvain.be)
 #
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -35,7 +35,7 @@ from admission.forms.translation_field import (
     TranslatedTextareasWidget,
     TranslatedValueField,
 )
-from base.models.enums.entity_type import DOCTORAL_COMMISSION
+from base.models.enums.organization_type import MAIN
 
 
 def default_category_labels():
@@ -233,7 +233,9 @@ class CddConfiguration(models.Model):
     cdd = models.OneToOneField(
         'base.Entity',
         on_delete=models.CASCADE,
-        limit_choices_to={'entityversion__entity_type': DOCTORAL_COMMISSION},
+        limit_choices_to={
+            'organization__type': MAIN,
+        },
         related_name='admission_config',
     )
     is_complementary_training_enabled = models.BooleanField(
