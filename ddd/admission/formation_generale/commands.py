88771ed8c98065e5004bfca39be0d83baad1166f
Gagaro <gagaro42@gmail.com>
2025-03-07 16:29:28 +0100

    Fix OS-1364: [V1.B] Back-office: Décision SIC > Gestion de la nouvelle dérogation déléguée/VRAE & Renommage dérogation existante pour distinguer les deux

--- ddd/admission/formation_generale/commands.py
+++ ddd/admission/formation_generale/commands.py
@@ -6,7 +6,7 @@
 #  The core business involves the administration of students, teachers,
 #  courses, programs and so on.
 #
-#  Copyright (C) 2015-2024 Université catholique de Louvain (http://www.uclouvain.be)
+#  Copyright (C) 2015-2025 Université catholique de Louvain (http://www.uclouvain.be)
 #
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -579,6 +579,15 @@ class SpecifierBesoinDeDerogationSicCommand(interface.CommandRequest):
     uuid_proposition: str
     besoin_de_derogation: str
     gestionnaire: str
+
+
+@attr.dataclass(frozen=True, slots=True)
+class SpecifierDerogationDelegueVraeSicCommand(interface.CommandRequest):
+    uuid_proposition: str
+    gestionnaire: str
+    derogation: str
+    commentaire: str = ''
+    justificatif: List[str] = attr.Factory(list)
 
 
 @attr.dataclass(frozen=True, slots=True)
