a906cca07091373f693fd2f0f58ba722a7cbf9d8
Gagaro <gagaro42@gmail.com>
2025-04-18 16:39:09 +0200

    Fix OS-1435: migrate schema generation to spectacular

--- tests/api/test_schema.py
+++ tests/api/test_schema.py
@@ -6,7 +6,7 @@
 #  The core business involves the administration of students, teachers,
 #  courses, programs and so on.
 #
-#  Copyright (C) 2015-2023 Université catholique de Louvain (http://www.uclouvain.be)
+#  Copyright (C) 2015-2025 Université catholique de Louvain (http://www.uclouvain.be)
 #
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -73,7 +73,7 @@ class ApiSchemaTestCase(TestCase):
                 ],
             )
             call_command(
-                'generateschema',
+                'spectacular',
                 urlconf='admission.api.url_v1',
                 generator_class='admission.api.schema.AdmissionSchemaGenerator',
                 file=temp.name,
