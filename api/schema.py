e9c567d2478fb4ce3fcce6f9ecdedb12971a5926
Gagaro <gagaro42@gmail.com>
2025-04-23 10:42:54 +0200

    Fix ActionLink schema

--- api/schema.py
+++ api/schema.py
@@ -134,6 +134,41 @@ class AdmissionSchemaGenerator(SchemaGenerator):
             },
             "required": ["code", "message"],
         }
+        schema['components']['schemas']['ActionLink'] = {
+            'type': 'object',
+            'properties': {
+                'error': {
+                    'type': 'string',
+                },
+                'method': {
+                    'type': 'string',
+                    'enum': [
+                        'DELETE',
+                        'GET',
+                        'PATCH',
+                        'POST',
+                        'PUT',
+                    ],
+                },
+                'url': {
+                    'type': 'string',
+                    'format': 'uri',
+                },
+            },
+            'oneOf': [
+                {
+                    'required': [
+                        'method',
+                        'url',
+                    ],
+                },
+                {
+                    'required': [
+                        'error',
+                    ],
+                },
+            ],
+        }
         schema['components']['schemas']['AcceptedLanguageEnum'] = {"type": "string", "enum": ["en", "fr-be"]}
         for path, path_content in schema['paths'].items():
             for method, method_content in path_content.items():
