4fb53c7ae77d730968415fb9863dc368b49741fb
Julien Cougnaud <julien.cougnaud@makina-corpus.com>
2025-05-07 17:20:57 +0200

    [OS-1383] Fixes after the performance improvements

--- infrastructure/admission/doctorat/preparation/repository/groupe_de_supervision.py
+++ infrastructure/admission/doctorat/preparation/repository/groupe_de_supervision.py
@@ -358,13 +358,18 @@ class GroupeDeSupervisionRepository(IGroupeDeSupervisionRepository):
             members.append(
                 klass(
                     uuid=str(actor.uuid),
-                    matricule=actor.person and actor.person.global_id,
+                    matricule=actor.person and actor.person.global_id or '',
                     nom=actor.last_name,
                     prenom=actor.first_name,
                     email=actor.email,
-                    est_docteur=True if not actor.is_external and hasattr(actor.person, 'tutor') else actor.is_doctor,
+                    est_docteur=(
+                        True
+                        if not actor.is_external and hasattr(actor.person, 'tutor')
+                        else actor.is_external and actor.is_doctor
+                    ),
                     institution=_('ucl') if not actor.is_external else actor.institute,
                     ville=actor.city,
+                    code_pays=actor.country_id and actor.country.iso_code or '',
                     pays=actor.country_id
                     and getattr(actor.country, 'name_en' if get_language() == 'en' else 'name')
                     or '',
