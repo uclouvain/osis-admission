6a4a70a1b456df6b096b9751084a86ab217b93db
Julien Cougnaud <julien.cougnaud@makina-corpus.com>
2025-05-28 10:57:10 +0200

    [OS-1474] Doctorate > checklist > cdd decision: update the predefined refusal reasons

--- migrations/0255_doctoraterefusalreasoncategory_and_more.py
+++ migrations/0255_doctoraterefusalreasoncategory_and_more.py
@@ -50,24 +50,15 @@ def populate_refusal_reasons(apps, schema_editor):
         'Résultats insuffisants': [
             "Vos résultats académiques ne satisfont pas aux critères d'admission.",
         ],
+        'Prérequis insuffisants': [
+            "Votre formation ne couvre pas les prérequis du programme visé.",
+            "La durée de votre formation est insuffisante.",
+        ],
         'Niveau de langue insuffisant/absent': [
             "Votre niveau de langue est insuffisant par rapport à celui exigé.",
             "La preuve du niveau de langue requis n'a pas été apportée.",
         ],
-        'Finançabilité': [
-            "Une dérogation pour non-finançabilité vous a déjà été accordée antérieurement. Malgré l’octroi de cette mesure de faveur, la faculté estime que le nombre de crédits acquis demeure insuffisant.",
-            "La faculté estime que vous n’apportez la preuve d’aucune circonstance exceptionnelle qui vous différencierait fondamentalement des autres étudiant·es et qui justifierait l’octroi d’une dérogation.",
-            "Au vu des résultats obtenus dans le cadre de votre parcours antérieur, la faculté estime qu’il est préférable d’envisager une réorientation vers d'autres types de formation.",
-            "La faculté estime que votre dossier ne comporte pas suffisamment d'éléments probants pour vous accorder une dérogation.",
-            "La faculté estime que les informations contenues dans votre courrier ne permettent pas de justifier les échecs précédents. Les éléments fournis ne lui permettent pas de s'assurer d'une amélioration de votre cursus universitaire.",
-            "Votre demande de dérogation a été introduite en dehors des délais fixés par la faculté conformément à l’article 22, al. 2 du Règlement général des études et des examens.",
-            "La faculté est sensible aux éléments évoqués dans votre courrier mais estime que vos chances de réussite sont trop faibles pour vous accorder une dérogation.",
-            "La faculté estime que votre rythme d'acquisition des crédits est trop faible pour pouvoir satisfaire les exigences en matière de finançabilité. Elle juge donc qu’il n’est pas raisonnable que vous poursuiviez ce projet de formation.",
-            "La faculté estime que vos résultats académiques sont manifestement insuffisants, et ce d’autant plus que ces résultats se rapportent à des unités d’enseignement importantes dans le programme de cycle.",
-            "Vous ne remplissez pas les conditions de finançabilité liées à la nationalité (art. 3).",
-            "Votre demande d'inscription vise des études qui ne donnent pas lieu à un financement (art. 2) - AESS / CAEAS",
-            "Vous ne remplissez plus les conditions académiques de finançabilité (art. 5).",
-        ],
+        'Finançabilité': [],
     }
 
     for category_order, (category_name, reasons) in enumerate(category_reasons.items()):
