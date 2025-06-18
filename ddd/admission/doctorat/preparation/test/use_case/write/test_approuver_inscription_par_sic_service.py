e318cc4b156a62e5bd9607e1b5d8be66ca2c655f
Julien Cougnaud <julien.cougnaud@makina-corpus.com>
2025-06-18 11:48:38 +0200

    [OS-1500] Checklist > to approve an admission, the personal data state must be validated

--- ddd/admission/doctorat/preparation/test/use_case/write/test_approuver_inscription_par_sic_service.py
+++ ddd/admission/doctorat/preparation/test/use_case/write/test_approuver_inscription_par_sic_service.py
@@ -6,7 +6,7 @@
 #    The core business involves the administration of students, teachers,
 #    courses, programs and so on.
 #
-#    Copyright (C) 2015-2024 Université catholique de Louvain (http://www.uclouvain.be)
+#    Copyright (C) 2015-2025 Université catholique de Louvain (http://www.uclouvain.be)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -33,26 +33,29 @@ from django.test import TestCase
 from admission.ddd.admission.doctorat.preparation.commands import (
     ApprouverInscriptionParSicCommand,
 )
-from admission.ddd.admission.doctorat.preparation.domain.model.enums import ChoixStatutPropositionDoctorale
-from admission.ddd.admission.doctorat.preparation.domain.model.enums.checklist import ChoixStatutChecklist
+from admission.ddd.admission.doctorat.preparation.domain.model.enums import (
+    ChoixStatutPropositionDoctorale,
+)
 from admission.ddd.admission.doctorat.preparation.domain.model.enums.checklist import (
-    OngletsChecklist,
     BesoinDeDerogation,
+    ChoixStatutChecklist,
+    OngletsChecklist,
 )
 from admission.ddd.admission.doctorat.preparation.domain.model.statut_checklist import (
     ORGANISATION_ONGLETS_CHECKLIST_PAR_STATUT,
 )
 from admission.ddd.admission.doctorat.preparation.domain.validator.exceptions import (
-    ParcoursAnterieurNonSuffisantException,
     DocumentAReclamerImmediatException,
     EtatChecklistDecisionSicNonValidePourApprouverUneInscription,
+    EtatChecklistDonneesPersonnellesNonValidePourApprouverDemande,
+    ParcoursAnterieurNonSuffisantException,
 )
 from admission.ddd.admission.doctorat.preparation.test.factory.groupe_de_supervision import (
     GroupeDeSupervisionSC3DPFactory,
 )
 from admission.ddd.admission.doctorat.preparation.test.factory.proposition import (
-    _PropositionIdentityFactory,
     PropositionAdmissionSC3DPAvecPromoteursEtMembresCADejaApprouvesFactory,
+    _PropositionIdentityFactory,
 )
 from admission.ddd.admission.domain.validator.exceptions import EnQuarantaineException
 from admission.ddd.admission.dtos.merge_proposal import MergeProposalDTO
@@ -68,12 +71,21 @@ from admission.infrastructure.admission.doctorat.preparation.repository.in_memor
 from admission.infrastructure.admission.doctorat.preparation.repository.in_memory.proposition import (
     PropositionInMemoryRepository,
 )
-from admission.infrastructure.admission.domain.service.in_memory.profil_candidat import ProfilCandidatInMemoryTranslator
-from admission.infrastructure.message_bus_in_memory import message_bus_in_memory_instance
+from admission.infrastructure.admission.domain.service.in_memory.profil_candidat import (
+    ProfilCandidatInMemoryTranslator,
+)
+from admission.infrastructure.message_bus_in_memory import (
+    message_bus_in_memory_instance,
+)
 from base.ddd.utils.business_validator import MultipleBusinessExceptions
 from base.models.person_merge_proposal import PersonMergeStatus
-from ddd.logic.shared_kernel.academic_year.domain.model.academic_year import AcademicYear, AcademicYearIdentity
-from infrastructure.shared_kernel.academic_year.repository.in_memory.academic_year import AcademicYearInMemoryRepository
+from ddd.logic.shared_kernel.academic_year.domain.model.academic_year import (
+    AcademicYear,
+    AcademicYearIdentity,
+)
+from infrastructure.shared_kernel.academic_year.repository.in_memory.academic_year import (
+    AcademicYearInMemoryRepository,
+)
 
 
 @freezegun.freeze_time('2020-11-01')
@@ -197,6 +209,15 @@ class TestApprouverInscriptionParSic(TestCase):
         with self.assertRaises(MultipleBusinessExceptions) as context:
             self.message_bus.invoke(self.command(**self.parametres_commande_par_defaut))
         self.assertIsInstance(context.exception.exceptions.pop(), ParcoursAnterieurNonSuffisantException)
+
+    def test_should_lever_exception_si_donnees_personnelles_non_validees(self):
+        self.proposition.checklist_actuelle.donnees_personnelles.statut = ChoixStatutChecklist.INITIAL_CANDIDAT
+        with self.assertRaises(MultipleBusinessExceptions) as context:
+            self.message_bus.invoke(self.command(**self.parametres_commande_par_defaut))
+        self.assertIsInstance(
+            context.exception.exceptions.pop(),
+            EtatChecklistDonneesPersonnellesNonValidePourApprouverDemande,
+        )
 
     def test_should_lever_exception_si_document_a_reclamer_immediatement(self):
         self.proposition.documents_demandes = {
