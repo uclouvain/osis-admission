71f39fd3b9c220ba06dd36afb3f376185a1ba17a
Julien Cougnaud <julien.cougnaud@makina-corpus.com>
2025-08-19 19:02:19 +0200

    [OS-1574] WIP

--- exports/admission_recap/attachments.py
+++ exports/admission_recap/attachments.py
@@ -27,6 +27,7 @@ from io import BytesIO
 from typing import Dict, List, Optional
 
 import img2pdf
+from PIL import Image, UnidentifiedImageError
 from django.utils.translation import override
 from osis_document.api.utils import get_raw_content_remotely
 from PIL.Image import DecompressionBombError
@@ -121,8 +122,31 @@ class Attachment:
                 return default_content
             if metadata.get('mimetype') in IMAGE_MIME_TYPES:
                 try:
-                    raw_content = img2pdf.convert(raw_content, rotation=img2pdf.Rotation.ifvalid)
-                except (DecompressionBombError, ValueError, img2pdf.ImageOpenError):
+                    with Image.open(BytesIO(raw_content)) as img:
+                        # Convert the image to RGB if necessary as img2pdf does not handle all cases
+                        with_transparency = 'transparency' in img.info
+                        if with_transparency or img.mode not in {'RGB', 'L'}:
+                            if img.mode in {'RGBA', 'LA', 'PA'} or with_transparency:
+                                # Set a white background
+                                background = Image.new('RGB', img.size, (255, 255, 255))
+                                img = Image.alpha_composite(background, img.convert('RGBA'))
+
+                            img = img.convert('RGB')
+
+                            with BytesIO() as out_buf:
+                                img.save(out_buf, format='PNG')
+                                raw_content = img2pdf.convert(
+                                    out_buf.getvalue(),
+                                    rotation=img2pdf.Rotation.ifvalid,
+                                    first_frame_only=True,
+                                )
+                        else:
+                            raw_content = img2pdf.convert(
+                                raw_content,
+                                rotation=img2pdf.Rotation.ifvalid,
+                                first_frame_only=True,
+                            )
+                except (DecompressionBombError, ValueError, img2pdf.ImageOpenError, UnidentifiedImageError):
                     # If the image size is too big or the image cannot be opened, display the default content
                     return default_content
             return BytesIO(raw_content)
